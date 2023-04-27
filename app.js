c0d81e8498d6ec0c193a36079b0cd71b565d14e0
stark5200 <a.taha5200@gmail.com>
2023-04-27 05:10:10 +0200

    Completed GET and POST

--- app.js
+++ app.js
@@ -14,3 +14,49 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 mongoose.connect(process.env.MONGO_SRV);
+
+const articleSchema = new mongoose.Schema({
+  title: String, 
+  content: String
+});
+
+const Article = new mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res){
+
+    Article.find(function(err, foundArticles){
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(foundArticles)
+      }
+    })
+});
+
+app.post("/articles", function(req, res){
+    console.log(req.body.title);
+    console.log(req.body.content);
+
+    const newArticle = new Article({
+      title: req.body.title, 
+      content: req.body.content
+    }); 
+
+    newArticle.save(function(err){
+      if (err) {
+        res.send(err);
+      } else {
+        res.send("Successfully added new article");
+      }
+    });
+})
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server Started at port 3000")
+});
